0945404f35c42c41274e6bd96d09baccddd4112b
Rodolfo Perez <rodolfobeste@gmail.com>
2022-11-12 21:15:52 +0200

    Configured config file to allow GCP image in Image component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/karlagarrido/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
